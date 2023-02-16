640c5560a922511356bb26fc760f65afd8181677
theRealViruzs <ali.kassim108@gmail.com>
2023-02-15 21:55:42 -0500

    added changes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Mesh } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
@@ -10,31 +11,69 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Object
- */
+// Create the particles
+var particleCount = 1000;
+var particlePositions = new Float32Array(particleCount * 3);
+var particleColors = new Float32Array(particleCount * 3);
+var particleSizes = new Float32Array(particleCount);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
-directionalLight.position.z = 5;
-directionalLight.position.x = 15;
+var color = new THREE.Color();
+
+for (var i = 0; i < particleCount; i++) {
+	particlePositions[i * 3 + 0] = Math.random() * 500 - 250;
+	particlePositions[i * 3 + 1] = Math.random() * 500 - 250;
+	particlePositions[i * 3 + 2] = Math.random() * 500 - 250;
+
+	color.setHSL(i / particleCount, 1.0, 0.5);
+
+	particleColors[i * 3 + 0] = color.r;
+	particleColors[i * 3 + 1] = color.g;
+	particleColors[i * 3 + 2] = color.b;
+
+	particleSizes[i] = 2;
+}
+
+var particleGeometry = new THREE.BufferGeometry();
+particleGeometry.setAttribute(
+	"position",
+	new THREE.BufferAttribute(particlePositions, 3)
+);
+particleGeometry.setAttribute(
+	"color",
+	new THREE.BufferAttribute(particleColors, 3)
+);
+particleGeometry.setAttribute(
+	"size",
+	new THREE.BufferAttribute(particleSizes, 1)
+);
+
+var particleMaterial = new THREE.PointsMaterial({
+	size: 2,
+	vertexColors: THREE.VertexColors,
+});
+
+var particleSystem = new THREE.Points(particleGeometry, particleMaterial);
+particleSystem.sortParticles = true;
+scene.add(particleSystem);
+
+// light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 scene.add(directionalLight);
 
-const texture = new THREE.TextureLoader().load("/normal.png");
+// texture
+const texture = new THREE.TextureLoader().load("/moon.jpg");
 
-// immediately use the texture for material creation
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshPhongMaterial({
-	color: 0xff0000,
+// creating my first figure
+const firstGeo = new THREE.SphereGeometry();
+const firstGeoMaterial = new THREE.MeshPhongMaterial({
+	shininess: 400,
+	reflectivity: 1,
+	color: 0x0000ff,
 	normalMap: texture,
-	shininess: 200,
-	reflectivity: 200,
 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const firstGeoMesh = new THREE.Mesh(firstGeo, firstGeoMaterial);
 
-/**
- * Sizes
- */
+scene.add(firstGeoMesh);
 const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight,
@@ -78,6 +117,7 @@ controls.enableDamping = true;
  */
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
+
 	antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
