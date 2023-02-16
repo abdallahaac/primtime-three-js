19afea69c819be15d25edf299654d53352a5d202
abdallahaac <aceydesignz@gmail.com>
2023-02-15 19:23:32 -0500

    added fullwidth

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Sizes
 const sizes = {
-	width: 800,
-	height: 600,
+	width: window.innerWidth,
+	height: window.innerHeight,
 };
 
 // Scene
